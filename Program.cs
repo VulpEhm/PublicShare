a76fea17ebafd6a2a658601cf9c6251c6af44577
VulpEhm <130221704+VulpEhm@users.noreply.github.com>
2023-05-12 20:43:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ping ping = new Ping();
+        string host = "google.com";
+        while (true)
+        {
+            try
+            {
+                PingReply reply = ping.Send(host);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Success");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Failure");
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Failure");
+                Console.ResetColor();
+            }
+            Thread.Sleep(1000); // wait for 1 second before sending the next ping
+        }
+    }
+}
